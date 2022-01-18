223feab0fb1e3d228b435d40e33abe8b797ba165
Mindaugas Vilkas <46817295+johnsnow0@users.noreply.github.com>
2022-01-18 09:36:44 +0200

    update

---
+++ EF_PAMOKA/Migrations/20220117185818_Vartotojai.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace EF_PAMOKA.Migrations
+{
+    public partial class Vartotojai : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vartotojai",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Vardas = table.Column<string>(type: "text", nullable: false),
+                    Slaptazodis = table.Column<string>(type: "text", nullable: false),
+                    Pastas = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vartotojai", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vartotojai");
+        }
+    }
+}
---
+++ EF_PAMOKA/Models/Vartotojas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EF_PAMOKA.Models
+{
+	public class Vartotojas
+	{
+		[Key]
+		public int ID { get; set; }
+		public string Vardas { get; set; }
+		public string Slaptazodis { get; set; }
+		public string Pastas { get; set; }
+	}
+}
+
